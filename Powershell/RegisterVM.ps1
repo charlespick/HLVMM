662b7f3cdb7a3bbcb9de92ec92a27b7a29911c8a
charlespick <me@charlespick.xyz>
2025-08-27 16:22:46 -0700

    moving on

--- Powershell/RegisterVM.ps1
+++ Powershell/RegisterVM.ps1
@@ -9,49 +9,45 @@ param (
     [int]$CPUcores,
 
     [Parameter(Mandatory=$true)]
-    [string]$VHDXPath,
+    [string]$VMDataFolder,
 
     [int]$VLANId = $null
 )
 
-# Extract the folder path of the VHDX file
-$VMFolderPath = Split-Path -Path $VHDXPath -Parent
-
-# Derive VMName from the parent folder of the VHDX path
-$VMName = Split-Path -Path $VMFolderPath -Leaf
-
 # Create the VM
-New-VM -Name $VMName -MemoryStartupBytes ($GBRam * 1GB) -Generation 2 -BootDevice VHD -Path $VMFolderPath
+$vm = New-VM -Name (Split-Path -Path $VMDataFolder -Leaf) -MemoryStartupBytes ($GBRam * 1GB) -Generation 2 -BootDevice VHD -Path (Split-Path -Path $VMDataFolder -Parent)
 
 # If the OS is Linux, set secure boot to Microsoft UEFI Certificate Authority
 if ($OSFamily -eq "Linux") {
-    Set-VMFirmware -VMName $VMName -SecureBootTemplate "MicrosoftUEFICertificateAuthority"
+    Set-VMFirmware -VM $vm -SecureBootTemplate "MicrosoftUEFICertificateAuthority"
 }
 
 # Configure RAM and CPU
-Set-VM -Name $VMName -DynamicMemoryEnabled $false
-Set-VMProcessor -VMName $VMName -Count $CPUcores
+Set-VM -VM $vm -DynamicMemoryEnabled $false
+Set-VMProcessor -VM $vm -Count $CPUcores
 
 # Mount the VHDX file
-Add-VMHardDiskDrive -VMName $VMName -Path $VHDXPath
+$VHDXPath = Get-ChildItem -Path $VMDataFolder -Filter *.vhdx -File | Select-Object -First 1
+Add-VMHardDiskDrive -VM $vm -Path $VHDXPath
 
 # Get the first Hyper-V network switch and attach the network adapter
 $NetworkSwitch = Get-VMSwitch | Select-Object -First 1
-Add-VMNetworkAdapter -VMName $VMName -SwitchName $NetworkSwitch.Name
+$adapter = Get-VMNetworkAdapter -VM $vm | Select-Object -First 1
+Set-VMNetworkAdapter -SwitchName $NetworkSwitch.Name -VMNetworkAdapter $adapter
 
 # If VLANId is set, configure VLAN on the network adapter
 if ($VLANId -ne $null) {
-    Set-VMNetworkAdapterVlan -VMName $VMName -Access -VlanId $VLANId
+    Set-VMNetworkAdapterVlan -VMNetworkAdapter $adapter -Access -VlanId $VLANId
 }
 
 # Check if an ISO file exists in the VM folder and mount it in a CD drive
-$ISOFile = Get-ChildItem -Path $VMFolderPath -Filter *.iso -File | Select-Object -First 1
+$ISOFile = Get-ChildItem -Path $VMDataFolder -Filter *.iso -File | Select-Object -First 1
 if ($ISOFile) {
-    Add-VMDvdDrive -VMName $VMName -Path $ISOFile.FullName
+    Add-VMDvdDrive -VM $vm -Path $ISOFile.FullName
 }
 
 # Set the first boot preference to the hard drive
-Set-VMFirmware -VMName $VMName -FirstBootDevice (Get-VMHardDiskDrive -VMName $VMName)
+Set-VMFirmware -VM $vm -FirstBootDevice (Get-VMHardDiskDrive -VM $vm )
 
 # Start the VM
-Start-VM -Name $VMName
+Start-VM -VM $vm
