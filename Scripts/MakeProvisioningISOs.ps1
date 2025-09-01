0181a137b2c0e2bb05b5c2743cf4c0b8e4ed30c7
charlespick <me@charlespick.xyz>
2025-08-31 22:04:00 -0700

    extend script to bake linux isos and bump version for recent host script updates

--- Scripts/MakeProvisioningISOs.ps1
+++ Scripts/MakeProvisioningISOs.ps1
@@ -1,14 +1,18 @@
 $oscdimgPath = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe"
 
 $repoRoot = (Get-Item -Path $PSScriptRoot).FullName | Split-Path -Parent
-$windowsFolder = Join-Path -Path $repoRoot -ChildPath "Windows"
 $isoOutputFolder = Join-Path -Path $repoRoot -ChildPath "ISOs"
-$isoOutputPath = Join-Path -Path $isoOutputFolder -ChildPath "WindowsProvisioning.iso"
-
 # Ensure the output directory exists
 if (-Not (Test-Path -Path $isoOutputFolder)) {
     New-Item -ItemType Directory -Path $isoOutputFolder | Out-Null
 }
 
-# Create the ISO
-& $oscdimgPath -m -o -u2 -udfver102 "$windowsFolder" "$isoOutputPath"
+$windowsFolder = Join-Path -Path $repoRoot -ChildPath "Windows"
+$winIsoOutputPath = Join-Path -Path $isoOutputFolder -ChildPath "WindowsProvisioning.iso"
+
+& $oscdimgPath -m -o -u2 -udfver102 "$windowsFolder" "$winIsoOutputPath"
+
+$linuxFolder = Join-Path -Path $repoRoot -ChildPath "Linux"
+$linuxIsoOutputPath = Join-Path -Path $isoOutputFolder -ChildPath "LinuxProvisioning.iso"
+
+& $oscdimgPath -m -o -u2 -lCIDATA "$linuxFolder" "$linuxIsoOutputPath"
